92dd5f96391f7bc74990806a40d1149ff33c90a4
M Aan Dafa Setiawan <kamikazesetan@gmail.com>
2022-10-13 10:22:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,47 +1,6 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Bootstrap demo</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-  <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-
-<body>
-
-  <nav class="navbar fixed-top">
-    <div class="container d-flex justify-content-between">
-      <div class="hamburger-button menu-t1">
-        <a type="button" href="#" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-
-          <h3 class="text-menu">MENU</h3>
-        </a>
-      </div>
-
-      <div class="menu-t2">
-        <button type="button" class="btn btn-primary btn-register">Register</button>
-        <button type="button" class="btn btn-primary btn-masuk">Masuk</button>
-      </div>
-    </div>
-  </nav>
-
-  <!-- offcanvas -->
-  <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
-    <div class="offcanvas-header">
-      <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdrop with scrolling</h5>
-      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-    </div>
-    <div class="offcanvas-body">
-      <p>Try scrolling the rest of the page to see this option in action.</p>
-    </div>
-  </div>
+<?php
+include('header.php');
+?>
 
   <!-- logo -->
   <div class="container">
@@ -148,7 +107,7 @@
               <div class="card-body">
                 <h5 class="card-title">Lorem, ipsum dolor sit amet consectetur adipisicing elit ?</h5>
                 <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ducimus, architecto laboriosam possimus corrupti eos aliquid nemo quas voluptate iste!</p>
-                <a href="#" class="btn btn-primary btn-artikel">Read more <i class="fa-solid fa-arrow-right"></i></a>
+                <a href="artikel.php" class="btn btn-primary btn-artikel">Read more <i class="fa-solid fa-arrow-right"></i></a>
               </div>
             </div>
           </div>
@@ -254,119 +213,7 @@
 
     </div>
 
-
-
-    <footer>
-      <div class="row">
-        <div class="col-sm-4 footerInfo text-center">
-          <img src="https://akcdn.detik.net.id/community/media/visual/2020/09/17/logo-detiknews.png?d=1" title="DetikNews" alt="detikNews">
-
-          <div class="sosmed">
-            <p>Connect With Us</p>
-            <i class="fa-brands fa-facebook fb"></i>
-            <i class="fa-brands fa-facebook fb"></i>
-            <i class="fa-brands fa-facebook fb"></i>
-            <i class="fa-brands fa-facebook fb"></i>
-            <i class="fa-brands fa-facebook fb"></i>
-          </div>
-        </div>
-
-        <div class="col-sm-2 footerMenu">
-          <h4>Kategori</h4>
-          <ul>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-          </ul>
-        </div>
-
-        <div class="col-sm-2 footerMenu">
-          <h4>Layanan</h4>
-          <ul>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-          </ul>
-        </div>
-
-        <div class="col-sm-2 footerMenu">
-          <h4>Informasi</h4>
-          <ul>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-          </ul>
-        </div>
-
-        <div class="col-sm-2 footerMenu">
-          <h4>Jaringan Media</h4>
-          <ul>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-          </ul>
-        </div>
-
-      </div>
-    </footer>
-
-
-  </div>
-
-
-
-
-
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-</body>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-<script>
-  $(function() {
-    $('.hamburger-button').on('click', function(event) {
-      event.preventDefault();
-      $(this).toggleClass('active');
-      checkMenu = $('.text-menu').text();
-
-      $('div.offcanvas-backdrop.fade.show').on('click', function() {
-        $('.hamburger-button').removeClass('active');
-        $('.text-menu').text('MENU');
-      });
-
-      if (checkMenu == 'MENU') {
-        $('.text-menu').text('TUTUP');
-      } else {
-        $('.text-menu').text('MENU');
-      }
-
-    });
-  });
-
-  $('.btn-close').on('click', function() {
-    $('.hamburger-button').removeClass('active');
-    $('.text-menu').text('MENU');
-  });
-</script>
-
-</html>
+<?php
+include('footer.php');
+?>
+    
