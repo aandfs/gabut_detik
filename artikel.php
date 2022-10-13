6b7d3190917cc0eace5f810313065f2828a57d0a
M Aan Dafa Setiawan <kamikazesetan@gmail.com>
2022-10-13 10:22:27 +0700

    Create artikel.php

---
+++ artikel.php
@@ -0,0 +1,225 @@
+<?php
+include('header.php');
+?>
+
+  <!-- logo -->
+  <div class="container">
+    <div class="grid-row no-gutter flex-between">
+      <div class="header_logo">
+        <a href="https://news.detik.com"><img src="https://akcdn.detik.net.id/community/media/visual/2020/09/17/logo-detiknews.png?d=1" class="img-fluid" title="DetikNews" alt="detikNews"></a>
+      </div>
+    </div>
+
+    <ul class="nav navbar-first">
+      <li class="bg-light">
+        <a class="navbar-brand" href="#">
+          <img src="https://akcdn.detik.net.id/community/media/visual/2020/09/17/logo-detiknews.png?d=1" alt="Bootstrap" width="100%" height="20px">
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" aria-current="page" href="#">Home</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" aria-current="page" href="#">Berita</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" aria-current="page" href="#">Jabotabek</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" aria-current="page" href="#">International</a>
+      </li>
+    </ul>
+
+    <ul class="nav navbar-second">
+      <li class="bg-light">
+        <a class="navbar-brand" href="#">
+          <img src="https://akcdn.detik.net.id/community/media/visual/2020/09/17/logo-detiknews.png?d=1" alt="Bootstrap" width="100%" height="20px">
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" aria-current="page" href="#">Home</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" aria-current="page" href="#">Berita</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" aria-current="page" href="#">Jabotabek</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" aria-current="page" href="#">International</a>
+      </li>
+    </ul>
+
+
+    <!-- top rekomendasi -->
+    <div class="row pt-3">
+      <div class="col-sm-3">
+        <div class="card artikel-sub">
+          <img src="https://source.unsplash.com/random/featured/?news" class="card-img-top">
+          <div class="card-body">
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quo.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-3">
+        <div class="card artikel-sub">
+          <img src="https://source.unsplash.com/random/featured/?news" class="card-img-top">
+          <div class="card-body">
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quo.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-3">
+        <div class="card artikel-sub">
+          <img src="https://source.unsplash.com/random/featured/?news" class="card-img-top">
+          <div class="card-body">
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quo.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-3">
+        <div class="card artikel-sub">
+          <img src="https://source.unsplash.com/random/featured/?news" class="card-img-top">
+          <div class="card-body">
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quo.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="pt-4">
+      <div class="row">
+
+        <!-- menu -->
+        <div class="col-sm-9">
+          <div class="col-sm-12">
+            <div class="card artikel">
+              <img src="https://source.unsplash.com/random/featured/?rapper" class="card-img-top">
+                <h5 class="text-judul card-title">Lorem, ipsum dolor sit amet consectetur adipisicing elit ?</h5>
+                <p class="text-isi">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam modi odit dicta reiciendis velit, corporis quibusdam rem nobis, aspernatur, ratione expedita pariatur id nesciunt nisi aliquam numquam eligendi? Nulla placeat dolores soluta libero perspiciatis, quas, fuga quaerat expedita impedit commodi explicabo ut. Voluptatum tempore perspiciatis corporis fugit suscipit obcaecati cumque repellendus nostrum odit, necessitatibus ducimus eveniet rem sunt dolores? Et mollitia minima necessitatibus in cupiditate animi nesciunt, harum reprehenderit consequatur eius adipisci, expedita illo maiores fuga vitae? Quae corporis et commodi voluptas est recusandae ullam ab dolor quia ratione tempora vero dignissimos officiis eius distinctio aliquid soluta accusantium, nihil facilis. Dolor, quasi suscipit, vitae soluta repellendus, reprehenderit commodi vel assumenda ducimus quos corporis adipisci nulla perferendis? Obcaecati numquam exercitationem, tenetur mollitia maxime animi deserunt deleniti aspernatur sint quisquam laboriosam dignissimos atque magni debitis voluptas, esse amet labore quam architecto! Fugit tenetur ad et asperiores ab sint minus ducimus commodi tempore.</br></br>
+            
+            
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam modi odit dicta reiciendis velit, corporis quibusdam rem nobis, aspernatur, ratione expedita pariatur id nesciunt nisi aliquam numquam eligendi? Nulla placeat dolores soluta libero perspiciatis, quas, fuga quaerat expedita impedit commodi explicabo ut. Voluptatum tempore perspiciatis corporis fugit suscipit obcaecati cumque repellendus nostrum odit, necessitatibus ducimus eveniet rem sunt dolores? Et mollitia minima necessitatibus in cupiditate animi nesciunt, harum reprehenderit consequatur eius adipisci, expedita illo maiores fuga vitae? Quae corporis et commodi voluptas est recusandae ullam ab dolor quia ratione tempora vero dignissimos officiis eius distinctio aliquid soluta accusantium, nihil facilis. Dolor, quasi suscipit, vitae soluta repellendus, reprehenderit commodi vel assumenda ducimus quos corporis adipisci nulla perferendis? Obcaecati numquam exercitationem, tenetur mollitia maxime animi deserunt deleniti aspernatur sint quisquam laboriosam dignissimos atque magni debitis voluptas, esse amet labore quam architecto! Fugit tenetur ad et asperiores ab sint minus ducimus commodi tempore.</br></br>
+            
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam modi odit dicta reiciendis velit, corporis quibusdam rem nobis, aspernatur, ratione expedita pariatur id nesciunt nisi aliquam numquam eligendi? Nulla placeat dolores soluta libero perspiciatis, quas, fuga quaerat expedita impedit commodi explicabo ut. Voluptatum tempore perspiciatis corporis fugit suscipit obcaecati cumque repellendus nostrum odit, necessitatibus ducimus eveniet rem sunt dolores? Et mollitia minima necessitatibus in cupiditate animi nesciunt, harum reprehenderit consequatur eius adipisci, expedita illo maiores fuga vitae? Quae corporis et commodi voluptas est recusandae ullam ab dolor quia ratione tempora vero dignissimos officiis eius distinctio aliquid soluta accusantium, nihil facilis. Dolor, quasi suscipit, vitae soluta repellendus, reprehenderit commodi vel assumenda ducimus quos corporis adipisci nulla perferendis? Obcaecati numquam exercitationem, tenetur mollitia maxime animi deserunt deleniti aspernatur sint quisquam laboriosam dignissimos atque magni debitis voluptas, esse amet labore quam architecto! Fugit tenetur ad et asperiores ab sint minus ducimus commodi tempore.</br></br></p>
+
+                <div class="d-flex justify-content-end">
+                    <a href="index.php" class="btn btn-primary btn-artikel text-right">Back <i class="fa-solid fa-arrow-right"></i></a>
+                </div>
+            </div>
+          </div>
+
+
+          <div class="pt-3">
+            <h4 class="tag">Rekomendasi untuk Anda</h4>
+          </div>
+
+          <div class="row">
+            <div class="col-sm-4 pt-2">
+              <div class="card artikel-sub">
+                <img src="https://source.unsplash.com/random/featured/?news" class="card-img-top">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quo.</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-sm-4 pt-2">
+              <div class="card artikel-sub">
+                <img src="https://source.unsplash.com/random/featured/?news" class="card-img-top">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quo.</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-sm-4 pt-2">
+              <div class="card artikel-sub">
+                <img src="https://source.unsplash.com/random/featured/?news" class="card-img-top">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quo.</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-sm-4 pt-2">
+              <div class="card artikel-sub">
+                <img src="https://source.unsplash.com/random/featured/?news" class="card-img-top">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quo.</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-sm-4 pt-2">
+              <div class="card artikel-sub">
+                <img src="https://source.unsplash.com/random/featured/?news" class="card-img-top">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quo.</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-sm-4 pt-2">
+              <div class="card artikel-sub">
+                <img src="https://source.unsplash.com/random/featured/?news" class="card-img-top">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quo.</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <!-- sidemenu -->
+        <div class="col-sm-3 sidemenu">
+          <div class="col-sm-12 ">
+            <h4 class="tag">Tag Populer</h4>
+            <ul>
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Lorem ipsum dolor sit.</li>
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Lorem ipsum dolor sit.</li>
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Lorem ipsum dolor sit.</li>
+            </ul>
+          </div>
+
+          <div class="col-sm-12">
+            <h4 class="tag">Komentar Terbanyak</h4>
+            <ul>
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Lorem ipsum dolor sit.</li>
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Lorem ipsum dolor sit.</li>
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Lorem ipsum dolor sit.</li>
+            </ul>
+          </div>
+
+          <div class="col-sm-12 sticky-top">
+            <h4 class="tag">Suara Pembaca</h4>
+            <div class="suara-pembaca">
+              <p>Kirimkan tanggapan dan komentar Anda yang berkaitan dengan pelayanan publik dan keluhan konsumen.</p>
+              <button class="btn btn-primary btn-sm btn-suara">Kirim Suara Anda <i class="fa-solid fa-arrow-right"></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+<?php
+include('footer.php');
+?>
+    
